a3ad7f331b8542850f64aa515874569211413d9e
Balagopal Ambalakkat <bambalakkat@gmail.com>
2013-01-30 18:00:30 -0800

    Code cleanup, indentation etc

--- routes/qwizbook.js
+++ routes/qwizbook.js
@@ -14,113 +14,113 @@ var Qwizbook = require('../models/Qwizbook');
 
 module.exports = {
 
-	createBook : function(req, res) {
+    createBook:function (req, res) {
 
-		var sessionUser = req.user;
-		var book = req.body;
+        var sessionUser = req.user;
+        var book = req.body;
 
-		Qwizbook.createQwizbook(sessionUser, book, function(err, book) {
-			// If error send the error response
-			if (err) {
-				res.send(400, err);
-				console.log(err);
-				return;
-			}
-			// No error send the unique ID for the newly created
-			// book.
-			console.log("QwizBook Added:");
-			console.log(JSON.stringify(book));
-			res.send({
-				id : book._id
-			});
-			//res.send({id:book.id});
+        Qwizbook.createQwizbook(sessionUser, book, function (err, book) {
+            // If error send the error response
+            if (err) {
+                res.send(400, err);
+                console.log(err);
+                return;
+            }
+            // No error send the unique ID for the newly created
+            // book.
+            console.log("QwizBook Added:");
+            console.log(JSON.stringify(book));
+            res.send({
+                id:book._id
+            });
+            //res.send({id:book.id});
 
-		})
-	},
+        })
+    },
 
-	getbook : function(req, res) {
+    getbook:function (req, res) {
 
-		console.log(req.user);
-	},
+        console.log(req.user);
+    },
 
-	getbooks : function(req, res) {
+    getbooks:function (req, res) {
 
-		var sessionUser = req.user;
+        var sessionUser = req.user;
 
-		if (req.query) {
+        if (req.query) {
 
-			var p = req.query;
-			var searchfilterArr = new Array();
-			for (var i in p) {
+            var p = req.query;
+            var searchfilterArr = new Array();
+            for (var i in p) {
 
-				searchfilterArr[i] = p[i];
-				//console.log("Request Request Det" + p[i]);
-			}
+                searchfilterArr[i] = p[i];
+                //console.log("Request Request Det" + p[i]);
+            }
 
-			var searchstring = searchfilterArr['search_str'];
-			var filterstring = searchfilterArr['sort_by'];
+            var searchstring = searchfilterArr['search_str'];
+            var filterstring = searchfilterArr['sort_by'];
 
-			if (searchstring) {
-				//console.log("search entered !!!!!");
-				Qwizbook.retrieveQwizbooksOnSearch(sessionUser, searchstring, filterstring, function(err, books) {
-					// If error send the error response
-					if (err) {
-						res.send(400, err);
-						console.log(err);
-						return;
-					}
-					// No error send the unique ID for the newly created book
+            if (searchstring) {
+                //console.log("search entered !!!!!");
+                Qwizbook.retrieveQwizbooksOnSearch(sessionUser, searchstring, filterstring, function (err, books) {
+                    // If error send the error response
+                    if (err) {
+                        res.send(400, err);
+                        console.log(err);
+                        return;
+                    }
+                    // No error send the unique ID for the newly created book
 
-					console.log("searched criteria" + JSON.stringify(books));
-					console.log("searched criteria num " + books.length);
-					res.send(JSON.stringify(books));
+                    console.log("searched criteria" + JSON.stringify(books));
+                    console.log("searched criteria num " + books.length);
+                    res.send(JSON.stringify(books));
 
-				})
-			} else {
+                })
+            } else {
 
-				Qwizbook.retrieveQwizbooksOnFilter(sessionUser, filterstring, function(err, books) {
-					// If error send the error response
-					if (err) {
-						res.send(400, err);
-						console.log(err);
-						return;
-					}
-					// No error send the unique ID for the newly created book
+                Qwizbook.retrieveQwizbooksOnFilter(sessionUser, filterstring, function (err, books) {
+                    // If error send the error response
+                    if (err) {
+                        res.send(400, err);
+                        console.log(err);
+                        return;
+                    }
+                    // No error send the unique ID for the newly created book
 
-					console.log("Filter criteria" + JSON.stringify(books));
-					console.log("searched criteria num " + books.length);
-					res.send(JSON.stringify(books));
+                    console.log("Filter criteria" + JSON.stringify(books));
+                    console.log("searched criteria num " + books.length);
+                    res.send(JSON.stringify(books));
 
-				})
-			}
+                })
+            }
 
-		} else {
+        } else {
 
-			Qwizbook.retrieveQwizbooks(sessionUser, function(err, books) {
-				// If error send the error response
-				if (err) {
-					res.send(400, err);
-					console.log(err);
-					return;
-				}
-				// No error send the unique ID for the newly created book
+            Qwizbook.retrieveQwizbooks(sessionUser, function (err, books) {
+                // If error send the error response
+                if (err) {
+                    res.send(400, err);
+                    console.log(err);
+                    return;
+                }
+                // No error send the unique ID for the newly created book
 
-				res.send(JSON.stringify(books));
+                res.send(JSON.stringify(books));
 
-			})
-		}
+            })
+        }
 
-	},
+    },
 
-	updateBook : function(req, res) {
-		console.log(req.user);
-	},
+    updateBook:function (req, res) {
+        console.log(req.user);
+    },
 
-	deleteBook : function(req, res) {
-		console.log(req.user);
-	},
+    deleteBook:function (req, res) {
+        console.log(req.user);
+    },
 
-	deleteBooks : function(req, res) {
-		console.log(req.user);
-	}
+    deleteBooks:function (req, res) {
+        console.log(req.user);
+    }
 };
