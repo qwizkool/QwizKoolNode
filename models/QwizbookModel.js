9d5d8b4ec8eaacc2238d40e4a16d92507794cbe9
Vinod <kgvinod@gmail.com>
2013-03-14 07:17:32 -0700

    Cleanup server side qwizbook model

---
+++ models/QwizbookModel.js
@@ -0,0 +1,38 @@
+/*!
+ * Copyright(c) 2013 Vibrentt 
+ *
+ * Module : QwizbookModel 
+ *
+ */
+
+/**
+ * Module dependencies.
+ */
+var db = require('../lib/db_connection')
+  , qwizbookSchema = require('./QwizbookSchema');
+
+/**
+ * Qwizbook model.
+ */
+
+// Create mongoose schema
+var qwizbookSchema = db.Schema(qwizbookSchema);
+
+qwizbookSchema.methods.getQwizbookForResponse = function () {
+
+    return {
+        title: this.title,
+        description: this.description,
+        id: this._id,
+        userrating:this.userrating            
+    };
+};
+
+// Get the mongoose model
+var QwizbookModel = db.conn.model('Qwizbook', qwizbookSchema);
+
+
+/**
+  * Exports.
+  */
+module.exports  = exports = QwizbookModel;
