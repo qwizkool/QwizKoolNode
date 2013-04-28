d0d75d4b30d176a556ba2e2de739f9d3c3fa3c44
Vinod <kgvinod@gmail.com>
2013-04-27 23:34:46 -0700

    Add SCION (scxml) library

--- public/app/config.js
+++ public/app/config.js
@@ -9,21 +9,12 @@ require.config({
 		// Put paths here.<script src="js/bootstrap.min.js"></script>
 
 		bootstrap : "../vendor/bootstrap/js/bootstrap.min",
-        bootstrap_select : "../vendor/bootstrap-select/bootstrap-select.min",
+        	bootstrap_select : "../vendor/bootstrap-select/bootstrap-select.min",
 		bootstrapValidation : "../vendor/bootstrap/js/jqBootstrapValidation",
 		tabs : "../assets/js/libs/tabs",
 		sha256 : "../assets/js/libs/sha256",
 		sha512 : "../assets/js/libs/sha512",
-		scion : "../assets/js/libs/SCION/lib/browser/SCXML",
-		core_scxml : "../assets/js/libs/SCION/lib/core/scxml/SCXML",
-		jsonml_dom : "../assets/js/libs/SCION/lib/external/jsonml/jsonml-dom",
-		annotate_scxml_json : "../assets/js/libs/SCION/lib/core/util/annotate-scxml-json",
-		json2model : "../assets/js/libs/SCION/lib/core/scxml/json2model",
-		array_set : "../assets/js/libs/SCION/lib/core/scxml/set/ArraySet",
-		state_kinds_enum : "../assets/js/libs/SCION/lib/core/scxml/state-kinds-enum",
-		setup_default_opts : "../assets/js/libs/SCION/lib/core/scxml/setup-default-opts",
-		scxml_dynamic_name_match_transition_selector : "../assets/js/libs/SCION/lib/core/scxml/scxml-dynamic-name-match-transition-selector",
-		scxml_model : "../assets/js/libs/SCION/lib/core/scxml/model"
+		scion : "../vendor/SCION/scion"
 
 	},
 	map : {
