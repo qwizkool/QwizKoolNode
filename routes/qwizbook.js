ddb219742f003a329f3ab521c434380af2c5bcf9
InduC <indu.c@pumexinfotech.com>
2013-04-29 18:15:01 +0530

    captcha using express jade

--- routes/qwizbook.js
+++ routes/qwizbook.js
@@ -147,72 +147,9 @@ Error : "Cannot rate Qwizbook "
 
 })
 } else {
-<<<<<<< HEAD
-/*
-Qwizbook.retrieveQwizbooksOnFilter(sessionUser, filterstring, function(err, books) {
-// If error send the error response
-var book_length = books.length;
-var c = 1;
-if (err) {
-res.send(400, err);
-console.log(err);
-return;
-}
-// No error send the unique ID for the newly created book
 
-//console.log("Filter criteria" + JSON.stringify(books));
-if(book_length>0)
-{
-var json ='[';
-var istrue =false;
-for (i=0;i<book_length;i++) {
-qbook = books[i];
-//console.log("routes qwizbook Id" + qbook._id);
-//bookIdDateSortArr[i] = qbook._id;
-//console.log("qwizbook retreived" + qbook);
-var userEmail = sessionUser.email;
-//QwizbookRating.getQwizbookRating(qbook,userEmail, function(err, book) {
-QwizbookRating.getQwizbookRating(qbook,userEmail, function(err, book) {
-if (err) {
-console.log(err);
-res.send(400, err);
-return;
-} else {
-if(istrue)
-{
-json +=',';
-}
-else
-{
-istrue = true;
-}
-json += book;
-if(c==book_length)
-{
-json += ']';
-res.send(json);
-}
-}
-c++;
-});
-
-}
-}
-else
-{
-res.send({
-Error : "Cannot rate Qwizbook "
-}, null);
-}
-
-});
-}
-*/
-
-Qwizbook.retrieveQwizbooksOnFilter(sessionUser, filterstring, function(err, books) {
-=======
 Qwizbook.retrieveQwizbooksOnFilter(sessionUser, filterstring, page,limit,function(err, books) {
->>>>>>> c5e11c6453430cb5631e9fc1c1ee450682da58a1
+
 // If error send the error response
 
 var book_length = books.length;
