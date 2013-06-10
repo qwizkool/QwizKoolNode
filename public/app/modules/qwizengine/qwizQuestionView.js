6c04c295da43b0727f03d39ffececfddb4892a10
Balagopal Ambalakkat <bambalakkat@gmail.com>
2013-06-10 08:00:12 -0700

    commit with test stub data used for testing partial rendering of question view

--- public/app/modules/qwizengine/qwizQuestionView.js
+++ public/app/modules/qwizengine/qwizQuestionView.js
@@ -31,7 +31,99 @@ define([
 
         render: function () {
 
-            this.$el.html(_.template(this.template, this.options.model.toJSON()));
+            //  Todo: FINAL CODE ,to be enabled once model is passed
+            // this.$el.html(_.template(this.template, this.options.model.toJSON()));
+
+            // Todo : --TEST STUB START--
+            var data = {
+                "title" :"Hello World",
+                "multiple_choice_question": {
+
+                    "question":{"text":"where is my dog?",
+                        "imageLinks" : [
+                            {
+                                "url" : "http://placehold.it/350x150",
+                                "_id" : "AAA",
+                                "description": "Hello"
+                            },
+                            {
+                                "url" : "../../assets/img/knowledge.gif",
+                                "_id" : "BBB",
+                                "description": "pulo"
+                            }
+                        ],
+                        "audioLinks" : [
+                            {
+                                "url" : " <iframe width='100%' height='166' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F47319680'></iframe>",
+                                "description": "Hello"
+                            },
+                            {
+                                "url" :" <iframe width='100%' height='166' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F47319680'></iframe>",
+                                "_id" : "BBB",
+                                "description": "pulo"
+                            }
+                        ],  "videoLinks" : [
+                            {
+                                "url" : "<iframe class='youtube-player' type='text/html' width='420' height='315' src='http://www.youtube.com/embed/k6U-i4gXkLM' frameborder='0'></iframe>",
+                                "_id" : "AAA",
+                                "description": "Hello"
+                            },
+                            {
+                                "url" : "<iframe src='http://player.vimeo.com/video/66177806' width='500' height='213' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> <p><a href='http://vimeo.com/66177806'>Cul-de-Sac</a> from <a href='http://vimeo.com/connorsimpson'>Connor Simpson</a> on <a href='http://vimeo.com'>Vimeo</a>.</p>",
+                                "_id" : "BBB",
+                                "description": "pulo"
+                            }
+                        ]
+                    },
+                    "answers" : [
+                        {
+                            "correct" : true,
+                            "_id" : "51b3da48092d9c921600001f",
+                            "choice" : {
+                                "text" : "rock",
+                                "audioLinks" : [ ],
+                                "imageLinks" : [ ],
+                                "videoLinks" : [ ]
+                            }
+                        },
+                        {
+                            "correct" : false,
+                            "_id" : "51b3da48092d9c921600001e",
+                            "choice" : {
+                                "text" : "scissors",
+                                "audioLinks" : [ ],
+                                "imageLinks" : [ ],
+                                "videoLinks" : [ ]
+                            }
+                        },
+                        {
+                            "correct" : false,
+                            "_id" : "51b3da48092d9c921600001d",
+                            "choice" : {
+                                "text" : "solid",
+                                "audioLinks" : [ ],
+                                "imageLinks" : [ ],
+                                "videoLinks" : [ ]
+                            }
+                        },
+                        {
+                            "correct" : false,
+                            "_id" : "51b3da48092d9c921600001c",
+                            "choice" : {
+                                "text" : "bottom",
+                                "audioLinks" : [ ],
+                                "imageLinks" : [ ],
+                                "videoLinks" : [ ]
+                            }
+                        }
+                    ]
+                }
+            };
+
+
+            this.$el.html(_.template(this.template, data));
+            // -- TEST STUB END--
+
             return this;
         },
 
